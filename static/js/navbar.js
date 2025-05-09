9614d4bff8bc6db11439ef4753bf0fdf235b4996
awerks <sustila24@gmail.com>
2025-05-09 21:57:52 +0200

    Add animation nav menu

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,20 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
+const navIcon = document.querySelector(".nav-toggle i");
+navToggle.addEventListener("click", function () {
+    const isOpen = navMenu.classList.toggle("nav-menu_visible");
 
-navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu_visible");
-    document.body.classList.toggle("menu-open", navMenu.classList.contains("nav-menu_visible"));
+    document.body.classList.toggle("menu-open", isOpen);
+    if (isOpen) {
+        this.setAttribute("aria-expanded", "true");
+        this.setAttribute("aria-label", "Close menu");
+        navIcon.classList.remove("fa-bars");
+        navIcon.classList.add("fa-times");
+    }
+    else {
+        this.setAttribute("aria-expanded", "false");
+        this.setAttribute("aria-label", "Open menu");
+        navIcon.classList.remove("fa-times");
+        navIcon.classList.add("fa-bars");
+    }
 });
