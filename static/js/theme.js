391ecef556c139d39c6949ede6a3edcabeb80d7c
awerks <sustila24@gmail.com>
2025-05-09 18:15:22 +0200

    Add spin animation

--- static/js/theme.js
+++ static/js/theme.js
@@ -27,6 +27,10 @@ function saveThemePreference(theme) {
 
 document.addEventListener("DOMContentLoaded", function () {
     const themeToggle = document.getElementById("theme-checkbox");
+    const spinMe = document.getElementById("spin-me");
+    if (spinMe) {
+        spinMe.addEventListener("click", spinFunction);
+    }
     if (themeToggle) {
         const currentTheme = localStorage.getItem("theme");
         if (document.documentElement.getAttribute("data-theme") === "dark" || currentTheme === "dark") {
@@ -45,3 +49,23 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+function spinFunction() {
+    const root = document.documentElement;
+    const spintime = 15 * 1000;
+    root.style.setProperty("--spintime", spintime + "ms");
+    root.classList.add("spin");
+    root.style.overflow = "hidden";
+    const onClick = () => cleanUp();
+    const timeoutId = setTimeout(cleanUp, spintime);
+
+    document.addEventListener("click", onClick, { once: true, capture: true });
+
+    function cleanUp() {
+        root.classList.remove("spin");
+        root.style.overflow = null;
+        root.style.setProperty("--spintime", null);
+        clearTimeout(timeoutId);
+        document.removeEventListener("click", onClick, { capture: true });
+    }
+}
